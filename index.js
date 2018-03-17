3989f78415a46bc912b56e932ccb4f761e7bb135
Munir Kizilcali <munir.kizilcali@gmail.com>
2018-03-17 23:01:29 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,24 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
+
+function nestedTarget() {
+  return document.querySelector(`#nested .target`)
+}
+
+function increaseRankBy(n) {
+  var ranks = document.querySelectorAll(`.ranked-list li`)
+  for (let i = 0; i < ranks.length; i++) {
+    ranks[i].innerText = parseInt(ranks[i].innerText)+n
+  }
+}
+
+function deepestChild() {
+  var current = document.querySelector("#grand-node")
+  var next = current
+while (next.childElementCount) {
+    current = next
+    next = next.children[0]
+  }
+  return next
+}
