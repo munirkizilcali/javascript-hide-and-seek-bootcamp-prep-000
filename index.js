09dc952b959485449af59ca3efc3af7634dba404
munirkizilcali <munir.kizilcali@gmail.com>
2018-06-05 17:09:03 -0400

    Done.

--- index.js
+++ index.js
@@ -3,25 +3,27 @@ function getFirstSelector(selector) {
 }
 
 function nestedTarget() {
-  return document.querySelector(`#nested .target`)
-}
-
-function increaseRankBy(n) {
-  var ranks = document.querySelectorAll(`.ranked-list`)
-  //const rankedLists = document.querySelectorAll('.ranked-list')
-  for (let i = 0; i < ranks.length; i++) {
-    for (let j = 0; j < ranks[i].children.length; j++) {
-      ranks[i].children[j].innerHTML = parseInt(ranks[i].children[j].innerHTML)+n
-    }
-  }
+  return document.querySelector("#nested .target")
 }
 
 function deepestChild() {
   var current = document.querySelector("#grand-node")
   var next = current
-while (next.childElementCount) {
+
+  while (Boolean(next.childElementCount)) {
+
     current = next
     next = next.children[0]
   }
   return next
+}
+
+function increaseRankBy(n) {
+  var rankedLists = document.querySelectorAll(".ranked-list")
+    for (let li = 0; li < rankedLists.length; li++){
+      for (let item = 0; item < rankedLists[li].children.length; item++) {
+        rankedLists[li].children[item].innerHTML = parseInt(rankedLists[li].children[item].innerHTML) + n
+      }
+    }
+  
 }
